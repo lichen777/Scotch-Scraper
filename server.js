a00a89a6009b3a1b9b3fe67149542765fc7df438
Lichen <LC@Lichens-MacBook-Air.local>
2018-03-12 08:03:59 -0500

    initial upload

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var logger = require('morgan')
+var mongoose = require('mongoose')
+
+var axios = require('axios')
+var cheerio = require('cheerio')
+
+var db = require('./models')
+
+var PORT = 8080
+
+var app = express()
+
+// Use morgan logger for logging requests
+app.use(logger('dev'))
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: false }))
+// Use express.static to serve the public folder as a static directory
+app.use(express.static('public'))
+
+// By default mongoose uses callbacks for async queries, we're setting it to use promises (.then syntax) instead
+// Connect to the Mongo DB
+mongoose.Promise = Promise
+mongoose.connect('mongodb://localhost/scotch', {
+  useMongoClient: true
+})
+
+// Routes
+
+
+// Start the server
+app.listen(PORT, function () {
+  console.log('App running on port ' + PORT + '!')
+})
