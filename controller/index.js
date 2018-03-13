0e0ca63f1fa53eefa2f24b2ae4183df3ddb25c26
unknown <Cli@ENG-CHEN-LP.domain.local>
2018-03-13 16:19:42 -0500

    try logging

--- controller/index.js
+++ controller/index.js
@@ -80,7 +80,7 @@ module.exports = {
   },
 
   unsaveArticle: function (req, res) {
-    console.log(req.body.id)
+    console.log(req.body)
     db.Article.findOneAndUpdate({ _id: req.body.id }, { $set: { saved: false } }, { new: true })
       .then(function (dbArticle) {
         // If we were able to successfully update an Article, send it back to the client
